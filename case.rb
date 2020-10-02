5bb0dba1aa5633a5957f1b1329d4878774255841
Andrew Park <acpark123@gmail.com>
2020-10-02 02:44:02 +0000

    Automatically backed up by Learn

---
+++ case.rb
@@ -0,0 +1,12 @@
+
+greeting = "friendly_greeing"
+
+case greeting
+
+ when "unfriendly_greeing"
+   puts "What do you want!?"
+
+ when "friendly_greeing"
+   puts "Hi! How are you?"
+
+end
---
+++ loop.rb
@@ -0,0 +1,9 @@
+
+count = 0
+
+until count >= 10
+
+  puts "The current number is #{count + 1}"
+  count += 1
+
+end
